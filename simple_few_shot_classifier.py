85347e7c284409ed3268279e7c4469b260fdc678
Arnault Chazareix <arnaultc@sicara.com>
2021-01-29 00:21:42 +0100

    add base script

---
+++ simple_few_shot_classifier.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+import pandas as pd
+
+from fsl_classifier import FSLClassifier
+from tensorflow_utils import image_dataset_from_paths
+
+fsl_classifier = FSLClassifier.example()
+
+
+def labeled_image_dataframe_from_folder(folder: Path) -> pd.DataFrame:
+    return pd.DataFrame(
+        [(str(p), p.parent.name) for p in folder.glob("*/*.jpg")],
+        columns=["image_path", "label"],
+    )
+
+
+catalog = labeled_image_dataframe_from_folder(Path("data/default_catalog"))
+fsl_classifier.set_catalog(
+    image_dataset_from_paths(list(catalog.image_path)), catalog.label
+)
+
+
+to_predict = labeled_image_dataframe_from_folder(Path("data/images"))
+predictions = fsl_classifier.predict(
+    image_dataset_from_paths(list(to_predict.image_path))
+)
+
+print(fsl_classifier.predictions_to_classes(predictions))
