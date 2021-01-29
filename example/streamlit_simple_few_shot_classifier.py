2b598a6f2ab80ce1ab5cea6fc3f67ceaf764fac8
Arnault Chazareix <arnaultc@sicara.com>
2021-01-29 01:27:55 +0100

    move examples in folder

--- example/streamlit_simple_few_shot_classifier.py
+++ example/streamlit_simple_few_shot_classifier.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 
 import streamlit as st
-from streamlit_utils import (
+from example.streamlit_utils import (
     labeled_image_dataframe_from_folder,
     columns_display,
     get_fsl_classifier_from_catalog_dataframe,
 )
 from tensorflow_utils import image_dataset_from_paths
 
-catalog = labeled_image_dataframe_from_folder(Path("data/default_catalog"))
+catalog = labeled_image_dataframe_from_folder(Path("../data/default_catalog"))
 
 st.title("Catalog")
 columns_display(list(zip(catalog.image_path, catalog.label)), n_columns=3)
@@ -16,7 +16,7 @@ columns_display(list(zip(catalog.image_path, catalog.label)), n_columns=3)
 
 fsl_classifier = get_fsl_classifier_from_catalog_dataframe(catalog)
 
-to_predict = labeled_image_dataframe_from_folder(Path("data/images"))
+to_predict = labeled_image_dataframe_from_folder(Path("../data/images"))
 predictions = fsl_classifier.predictions_to_classes(
     fsl_classifier.predict(image_dataset_from_paths(list(to_predict.image_path)))
 )
